e39f295d2dc47b856ce78d6cfa94c51a589a3f2c
MonkeYKonG <MonkeYKonG@users.noreply.github.com>
2019-01-29 23:25:59 +0100

    Adding Healthbar + Speed info to Hud
    The added comtent are not updated yet

--- Source/Palhu/HowTo_VehiculeHud.cpp
+++ Source/Palhu/HowTo_VehiculeHud.cpp
@@ -16,23 +16,56 @@
 
 AHowTo_VehiculeHud::AHowTo_VehiculeHud()
 {
-	static ConstructorHelpers::FObjectFinder<UFont> Font(TEXT("/Engine/EngineFonts/RobotoDistanceField"));
-	HUDFont = Font.Object;
 }
 
 void AHowTo_VehiculeHud::DrawHUD()
 {
 	Super::DrawHUD();
 
-	if (CrossHairTexture)
-	{
-		FVector2D Center(Canvas->ClipX * 0.5f, Canvas->ClipY * 0.5f);
-		FVector2D CrossHairDrawPosition(Center.X - (CrossHairTexture->GetSurfaceWidth() * 0.5f), Center.Y - (CrossHairTexture->GetSurfaceHeight() * 0.5f));
-		FCanvasTileItem TileItem(CrossHairDrawPosition, CrossHairTexture->Resource, FLinearColor::White);
+	DrawCrossHair();
+	DrawHealthBar();
+	DrawSpeedInfo();
+}
 
-		TileItem.BlendMode = SE_BLEND_Translucent;
-		Canvas->DrawItem(TileItem);
-	}
+void AHowTo_VehiculeHud::DrawCrossHair()
+{
+	if (CrossHairTexture == nullptr)
+		return;
+
+	FVector2D Center(Canvas->ClipX * 0.5f, Canvas->ClipY * 0.5f);
+	FVector2D CrossHairDrawPosition(Center.X - (CrossHairTexture->GetSurfaceWidth() * 0.5f), Center.Y - (CrossHairTexture->GetSurfaceHeight() * 0.5f));
+	FCanvasTileItem TileItem(CrossHairDrawPosition, CrossHairTexture->Resource, FLinearColor::White);
+
+	TileItem.BlendMode = SE_BLEND_Translucent;
+	Canvas->DrawItem(TileItem);
+}
+
+void AHowTo_VehiculeHud::DrawHealthBar()
+{
+	if (HealthBarBackgroundTexture == nullptr || HealthBarForegroundTexture == nullptr)
+		return;
+	FCanvasTileItem BackgroundTileItem(HealthBarOffset, HealthBarBackgroundTexture->Resource, HealthBarDimention, FLinearColor::White);
+	FCanvasTileItem ForegroundTileItem(HealthBarOffset + HealthBarForegroundOffset, HealthBarForegroundTexture->Resource, HealthBarDimention - 2 * HealthBarForegroundOffset, FLinearColor::White);
+
+	BackgroundTileItem.BlendMode = SE_BLEND_Translucent;
+	ForegroundTileItem.BlendMode = SE_BLEND_Translucent;
+	Canvas->DrawItem(BackgroundTileItem);
+	Canvas->DrawItem(ForegroundTileItem);
+}
+
+void AHowTo_VehiculeHud::DrawSpeedInfo()
+{
+	if (SpeedInfoFont == nullptr)
+		return;
+	FVector2D SpeedTextLocation(Canvas->ClipX - SpeedOffset.X, Canvas->ClipY - SpeedOffset.Y);
+	FVector2D GearsTextLocation(Canvas->ClipX - GearsOffset.X, Canvas->ClipY - GearsOffset.Y);
+	FCanvasTextItem SpeedTextItem(SpeedTextLocation, SpeedText, SpeedInfoFont, FLinearColor::White);
+	FCanvasTextItem GearsTextItem(GearsTextLocation, GearsText, SpeedInfoFont, FLinearColor::White);
+
+	SpeedTextItem.BlendMode = SE_BLEND_Translucent;
+	GearsTextItem.BlendMode = SE_BLEND_Translucent;
+	Canvas->DrawItem(SpeedTextItem);
+	Canvas->DrawItem(GearsTextItem);
 }
 
 #undef LOCTEXT_NAMESPACE
